9b89dad4b7cf1d0cb611ca4072a14d5d35f50a6c
Muhammad Erico Revaldo <email_github_erikorevaldo4@gmail.com>
2026-09-22 10:31:55 +0700

    Fix CORS for Flutter Web

--- api/simpan_ide.js
+++ api/simpan_ide.js
@@ -2,6 +2,15 @@
 // Deploy this file under /api on Vercel (it will be available at /api/simpan_ide)
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
