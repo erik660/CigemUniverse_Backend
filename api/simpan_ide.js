18cf12e9d0c422a688e6b399e71d805daac99e2f
Muhammad Erico Revaldo <email_github_erikorevaldo4@gmail.com>
2026-09-22 10:27:14 +0700

    Remove dotenv for vercel compatibility

--- api/simpan_ide.js
+++ api/simpan_ide.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 // Vercel Serverless function to generate content ideas using Groq API
 // Deploy this file under /api on Vercel (it will be available at /api/simpan_ide)
 
